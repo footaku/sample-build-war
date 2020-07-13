0d126d13184105b5eec67ccf749f7e4aa6d0c276
footaku <18702821+footaku@users.noreply.github.com>
2020-07-13 18:05:07 +0900

    init

---
+++ src/main/kotlin/com/example/footaku/samplebuildwar/SampleBuildWarApplication.kt
@@ -0,0 +1,11 @@
+package com.example.footaku.samplebuildwar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SampleBuildWarApplication
+
+fun main(args: Array<String>) {
+    runApplication<SampleBuildWarApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/footaku/samplebuildwar/SampleBuildWarApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.footaku.samplebuildwar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SampleBuildWarApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
